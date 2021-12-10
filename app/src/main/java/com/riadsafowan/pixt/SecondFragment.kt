e0a484d0313757ebad2a21e83710ed665874ef69
Riad Safowan <riad.safowan@northsouth.edu>
2021-12-10 15:10:30 +0600

    storage permission

--- app/src/main/java/com/riadsafowan/pixt/SecondFragment.kt
+++ app/src/main/java/com/riadsafowan/pixt/SecondFragment.kt
@@ -1,8 +1,10 @@
 package com.riadsafowan.pixt
 
-import android.content.ClipData
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,6 +17,8 @@ import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import android.graphics.Bitmap
 import android.os.Environment
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStream
@@ -32,6 +36,19 @@ class SecondFragment : Fragment() {
 //        val clipData = ClipData.newPlainText("image", null)
 //        clipboard.setPrimaryClip(clipData)
     }
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (!isGranted) {
+                Toast.makeText(
+                    requireContext(),
+                    "Permission is required to download this image",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    var lastClickTime: Long = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,12 +72,27 @@ class SecondFragment : Fragment() {
         binding.btnPaste.setOnClickListener(pasteClickLister)
         binding.tvPaste.setOnClickListener(pasteClickLister)
         binding.btnDownload.setOnClickListener {
+            if (System.currentTimeMillis() - lastClickTime < 2000) {
+                return@setOnClickListener
+            }
             saveToLocal()
+            lastClickTime = System.currentTimeMillis()
         }
     }
 
+    @SuppressLint("SimpleDateFormat")
     private fun saveToLocal() {
-        val folderPath = Environment.getExternalStorageDirectory().toString() + "/DCIM/Pixt/"
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
+
+        val folderPath = Environment.getExternalStorageDirectory().toString() + "/Pictures/Pixt/"
         val folder = File(folderPath)
         if (!folder.exists()) {
             File(folderPath).mkdirs()
